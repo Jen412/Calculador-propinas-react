0fbb465d4c15a01a82e5b55ee51f86018650d7fd
Codigo Con Juan <blackmaxxgdl18@hotmail.com>
2023-09-21 12:43:55 -0600

    05

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { menuItems } from "./data/db"
+
 
 function App() {
+
+  console.log(menuItems)
 
   return (
     <>
         <header className=" bg-teal-400 py-5">
           <h1 className="text-center text-4xl font-black">Calculadora de Propinas y Consumo</h1>
         </header>
+
+
     </>
   )
 }
