e9f5b4e1d45069af90b52d4cbc67928c8c3373f8
Codigo Con Juan <blackmaxxgdl18@hotmail.com>
2023-09-21 17:52:29 -0600

    13

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -5,11 +5,17 @@ export default function useOrder() {
     const [order, setOrder] = useState<OrderItem[]>([])
 
     const addItem = (item : MenuItem) => {
-       
-        const newItem  = {...item, quantity: 1}
-        setOrder([...order, newItem])
-
-
+        const itemExist = order.find(orderItem => orderItem.id === item.id)
+        if(itemExist) {
+            const updatedOrder = order.map( orderItem => orderItem.id === item.id ? 
+                {...orderItem, quantity: orderItem.quantity + 1 } : 
+                orderItem
+            )
+            setOrder(updatedOrder)
+        } else {
+            const newItem  = {...item, quantity: 1}
+            setOrder([...order, newItem])
+        }
     }
 
     console.log(order)
