d53deade241b936e7a9a960fb6867dea8969abdd
Codigo Con Juan <blackmaxxgdl18@hotmail.com>
2023-09-21 17:31:57 -0600

    11

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -1,11 +1,11 @@
 import { useState } from "react"
-import type { OrderItem } from "../types"
+import type { MenuItem, OrderItem } from "../types"
 
 export default function useOrder() {
     const [order, setOrder] = useState<OrderItem[]>([])
 
-    const addItem = () => {
-        console.log('agregando...')
+    const addItem = (item : MenuItem) => {
+        console.log(item)
     }
 
     return {
