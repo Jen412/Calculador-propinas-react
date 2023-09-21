3342958afb219c5b5927c03d6cf6abfb3749dc71
Codigo Con Juan <blackmaxxgdl18@hotmail.com>
2023-09-21 12:34:28 -0600

    04

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ function App() {
 
   return (
     <>
-        <h1 className=" text-8xl font-black">Hola Mundo</h1>
+        <header className=" bg-teal-400 py-5">
+          <h1 className="text-center text-4xl font-black">Calculadora de Propinas y Consumo</h1>
+        </header>
     </>
   )
 }
