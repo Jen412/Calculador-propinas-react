03dbe0c05cfa617b67ab8c1954ee88bce4c5495e
Codigo Con Juan <blackmaxxgdl18@hotmail.com>
2023-09-21 19:33:40 -0600

    15

--- src/components/OrderContents.tsx
+++ src/components/OrderContents.tsx
@@ -1,3 +1,4 @@
+import { formatCurrency } from "../helpers"
 import { OrderItem } from "../types"
 
 type OrderContentsProps = {
@@ -9,15 +10,29 @@ export default function OrderContents({order} : OrderContentsProps) {
     <div>
         <h2 className='font-black text-4xl'>Consumo</h2>
 
-        <div className="space-y-3 mt-5">
+        <div className="space-y-3 mt-10">
             {order.length === 0 ? 
                 <p className="text-center">La orden esta vacia</p>
             : (
                 order.map( item => (
-                    <div key={item.id}>
-                        <p>
-                            {item.name} - {item.price}
-                        </p>
+                    <div 
+                        key={item.id}
+                        className="flex justify-between items-center border-t border-gray-200 py-5 last-of-type:border-b"
+                    >
+                        <div>
+                            <p className="text-lg">
+                                {item.name} - {formatCurrency( item.price) }
+                            </p>
+                            <p className="font-black">
+                                Cantidad: {item.quantity} - { formatCurrency(item.price * item.quantity) }
+                            </p>
+                        </div>
+
+                        <button
+                            className="bg-red-600 h-8 w-8 rounded-full text-white font-black"
+                        >
+                            X
+                        </button>
                     </div>
                 ))
             )}
