7500662ca7c69d2dd7d73b224988a1ab73c4cffb
Codigo Con Juan <blackmaxxgdl18@hotmail.com>
2023-09-25 12:21:17 -0600

    22

--- src/components/OrderTotals.tsx
+++ src/components/OrderTotals.tsx
@@ -11,6 +11,7 @@ export default function OrderTotals({order, tip} : OrderTotalsProps) {
 
     const subtotalAmount = useMemo(() => order.reduce( (total, item) => total + (item.quantity * item.price), 0 ) , [order])
     const tipAmount = useMemo(() => subtotalAmount * tip, [tip, order])
+    const totalAmount = useMemo(() => subtotalAmount + tipAmount, [tip, order])
 
     return (
         <>
@@ -19,13 +20,11 @@ export default function OrderTotals({order, tip} : OrderTotalsProps) {
                 <p>Subtotal a pagar: {''}
                     <span className="font-bold">{ formatCurrency(subtotalAmount) }</span>
                 </p>
-
                 <p>Propina: {''}
                     <span className="font-bold">{ formatCurrency(tipAmount) }</span>
                 </p>
-
                 <p>Total a Pagar: {''}
-                    <span className="font-bold">$0</span>
+                    <span className="font-bold">{ formatCurrency(totalAmount) }</span>
                 </p>
             </div>
 
