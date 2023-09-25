77cbf5890bd708344717fd1e1ca2c0b513eb5186
Codigo Con Juan <blackmaxxgdl18@hotmail.com>
2023-09-25 12:30:37 -0600

    23

--- src/components/OrderTotals.tsx
+++ src/components/OrderTotals.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from "react"
+import { useMemo, useCallback } from "react"
 import { OrderItem } from "../types"
 import { formatCurrency } from "../helpers"
 
@@ -9,22 +9,22 @@ type OrderTotalsProps = {
 
 export default function OrderTotals({order, tip} : OrderTotalsProps) {
 
-    const subtotalAmount = useMemo(() => order.reduce( (total, item) => total + (item.quantity * item.price), 0 ) , [order])
-    const tipAmount = useMemo(() => subtotalAmount * tip, [tip, order])
-    const totalAmount = useMemo(() => subtotalAmount + tipAmount, [tip, order])
+    const subtotalAmount = useCallback(() => order.reduce( (total, item) => total + (item.quantity * item.price), 0 ) , [order])
+    const tipAmount = useCallback(() => subtotalAmount() * tip, [tip, order])
+    const totalAmount = useCallback(() => subtotalAmount() + tipAmount(), [tip, order])
 
     return (
         <>
             <div className="space-y-3">
                 <h2 className="font-black text-2xl">Totales y Propina:</h2>
                 <p>Subtotal a pagar: {''}
-                    <span className="font-bold">{ formatCurrency(subtotalAmount) }</span>
+                    <span className="font-bold">{ formatCurrency(subtotalAmount()) }</span>
                 </p>
                 <p>Propina: {''}
-                    <span className="font-bold">{ formatCurrency(tipAmount) }</span>
+                    <span className="font-bold">{ formatCurrency(tipAmount()) }</span>
                 </p>
                 <p>Total a Pagar: {''}
-                    <span className="font-bold">{ formatCurrency(totalAmount) }</span>
+                    <span className="font-bold">{ formatCurrency(totalAmount()) }</span>
                 </p>
             </div>
 
