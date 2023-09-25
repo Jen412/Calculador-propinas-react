0c7effbfe231113beedff2afc94bcb8d13bf17e3
Codigo Con Juan <blackmaxxgdl18@hotmail.com>
2023-09-25 11:10:08 -0600

    17

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import MenuItem from "./components/MenuItem"
 import OrderContents from "./components/OrderContents"
+import OrderTotals from "./components/OrderTotals"
 import { menuItems } from "./data/db"
 import useOrder from "./hooks/useOrder"
 
@@ -33,6 +34,8 @@ function App() {
               order={order}
               removeItem={removeItem}
             />
+
+            <OrderTotals />
 
           </div>
         </main>
